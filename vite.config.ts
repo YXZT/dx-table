c1908f66d68505b7c31eb741c10d0302d6ecd41c
ctgstw88/dongtao <1585232723@qq.com>
2023-03-13 11:06:45 +0800

    路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
     }
   },
   build:{
-    outDir:'docs'
+    outDir:'docs',
+    assetsDir:path.resolve(__dirname, './docs'),
   }
 })
