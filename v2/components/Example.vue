5b26fc617d4f38c36af95b378a4cc8ba23aaaa01
yxzt <1585232723@qq.com>
2023-09-07 21:56:03 +0800

    表格头部提示的封装

--- v2/components/Example.vue
+++ v2/components/Example.vue
@@ -190,6 +190,7 @@ const updateValueRole = (e: Array<any> | string | number | null, option: any, in
 };
 const isEdit = ref(true);
 // todo 增加配置 切换到该页面时，自动刷新表格
+// 表格头部提示的封装
 </script>
 
 <style scoped lang="scss">
