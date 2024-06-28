61b9c3b4c931353426b1feb77e4beac05d4257ef
ctgstw88/dongtao <1585232723@qq.com>
2024-06-28 17:38:23 +0800

    默认

--- v2/lib/TableExport.vue
+++ v2/lib/TableExport.vue
@@ -1,40 +1,78 @@
 <script setup lang='ts'>
-import { ref, toRefs, toValue } from 'vue';
-import { NForm, NFormItem, NInput, NCheckbox,NButton } from 'naive-ui';
+import { computed, ref, toRefs, toValue } from 'vue';
+import { NForm, NFormItem, NInput, NCheckbox, NButton } from 'naive-ui';
 import type { columnSetting, columnsSetting } from '@/interface';
-
+import { useThemeVars } from 'naive-ui'
 const props = defineProps<{
   dataSetting: columnsSetting,
   fileName: String,
 }>()
-const emit = defineEmits<{
+const emits = defineEmits<{
   (e: 'save', data: any): void
   (e: 'change-show', data: columnSetting): void
 }>()
 const saveName = ref(props.fileName)
 
-let localDataSetting = ref(props.dataSetting.map(ele=>({...ele})))
+let localDataSetting = computed(()=>props.dataSetting.map(ele => ({ ...ele })))
 
-function submitEvent(){
-  emit('save', {
+function submitEvent() {
+  emits('save', {
     dataSetting: localDataSetting,
     fileName: saveName.value,
   })
 }
-function changeColExport(e:any,col: columnSetting){
+function changeColExport(e: any, col: columnSetting) {
   col.isExport = e
-  emit('change-show', {...col})
+  emits('change-show', { ...col })
 }
+function colShowDefault() {
+  props.dataSetting.forEach(ele => {
+    if (ele._isExport) {
+      ele.isExport = true
+      emits('change-show', ele)
+    } else {
+      ele.isExport = false
+      emits('change-show', ele)
+    }
+  })
+}
+function colShowAll() {
+  props.dataSetting.forEach(ele => {
+    ele.isExport = true
+    emits('change-show', ele)
+  })
+}
+function colNotShowAll() {
+  props.dataSetting.forEach(ele => {
+    ele.isExport = false
+    emits('change-show', ele)
+  })
+}
+// todo换颜色
+const baseColor = useThemeVars().value.primaryColor
 // todo 模板化设置文件名，比如单号、公司名、日期等
 </script>
 <template>
   <div>
     <n-form label-placement="left" label-width="200px">
       <n-form-item label="导出字段设置：">
-        <div class="simple-table-sequence">
-          <div v-for="(item) in localDataSetting" :key="item.titleString" class="simple-table-sequence-item">
-            <n-checkbox :checked="item.isExport" :on-update:checked="(e: any)=>changeColExport(e, item)">
-              {{ item.titleString }}</n-checkbox>
+        <div style="width:100%">
+          <div>
+            <n-button quaternary type="info" @click="colShowDefault">
+              默认设置
+            </n-button>|
+            <n-button quaternary type="info" @click="colShowAll">
+              全选
+            </n-button>|
+            <n-button quaternary type="info" @click="colNotShowAll">
+              全不选
+            </n-button>
+          </div>
+          <div class="simple-table-sequence">
+            <div v-for="(item) in localDataSetting" :key="item.titleString" class="simple-table-sequence-item">
+              <n-checkbox :checked="item.isExport" :on-update:checked="(e: any) => changeColExport(e, item)" :class="{ 'default-export': item._isExport }">
+                {{ item.titleString }}</n-checkbox>
+            </div>
           </div>
         </div>
       </n-form-item>
@@ -67,6 +105,11 @@ function changeColExport(e:any,col: columnSetting){
 
   .simple-table-sequence-item:hover {
     box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.3);
+  }
+  .default-export{
+    :v-deep(.n-checkbox__label){
+      color: v-bind(baseColor) !important
+    }
   }
 }
 </style>
